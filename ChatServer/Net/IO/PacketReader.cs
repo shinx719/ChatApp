9719fceee7dfe3dcf0836ace3d2870be7df56c4d
erlan <erlan@SAD>
2025-04-04 18:45:45 +0300

    ser

---
+++ ChatServer/Net/IO/PacketReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer.Net.IO
+{
+    class PacketReader : BinaryReader
+    {
+        private NetworkStream _ns;
+
+        public PacketReader(NetworkStream ns) : base(ns)
+        {
+            _ns = ns;
+        }
+
+        public string ReadMessage()
+        {
+            int length = ReadInt32(); // Читаем длину сообщения
+            byte[] msgBuffer = ReadBytes(length); // Читаем сами байты
+
+            return Encoding.UTF8.GetString(msgBuffer); // Декодируем в строку
+        }
+    }
+}
