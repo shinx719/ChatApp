eb232bfa6d7377541b0bb90cd0327efb58145ae3
erlan <erlan@SAD>
2025-04-04 15:27:02 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ChatApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			MainWindow mainWindow1 = new MainWindow();
+			mainWindow1.Show();
+
+			MainWindow mainWindow2 = new MainWindow();
+			mainWindow2.Show();
+		}
+
+	}
+
+
+}
